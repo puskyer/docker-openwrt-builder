b14ad1ab6bd812f094c47f2223ce2ca2891140e8
BuildTools <unconfigured@null.spigotmc.org>
2025-01-21 10:35:56 -0500

    clean up of ubuntu 24.04 openwrt v24.10 cleanup

--- ubuntu-24.04/gorun_v24.10.0-rc5.sh
+++ ubuntu-24.04/gorun_v24.10.0-rc5.sh
@@ -2,6 +2,6 @@
 
 #docker run --interactive --rm --tty --ulimit 'nofile=1024:262144' --volume "$HOME"/build/archive/:/home/pusky/source/archive/ --volume "$HOME"/build/archive/source:/home/pusky/source/ --workdir '/home/pusky/source/'    ubuntu-24.04/openwrt:latest  /bin/bash
 docker run --interactive --rm --tty --ulimit 'nofile=1024:262144' \
-       --volume "$HOME"/build/archive:/home/pusky/archive/ \
-       --workdir '/home/pusky/source/' openwrt/ubuntu-24.04:latest  /bin/bash
+       --volume "$HOME"/build/archive/:/home/pusky/archive/ \
+       --workdir '/home/pusky/source/' openwrt/ubuntu-24.04:v24.10.0-rc5  /bin/bash
 
